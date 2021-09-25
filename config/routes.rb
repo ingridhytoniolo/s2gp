0a33be7f4b53eb4685c9e0b8da022517dd4e19ac
Renan Nelsen <renan@nelsen.com.br>
2021-09-25 14:56:21 -0300

    Add gem devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   root 'pages#index'
-
+  
+  devise_for :users
+  
   get 'pages/index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
