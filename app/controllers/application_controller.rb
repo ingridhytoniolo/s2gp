e5c9fa0bd90ad17c249668589b012358599f154b
Renan Nelsen <renan@nelsen.com.br>
2021-10-05 18:29:10 -0300

    Add group name

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,19 @@
 class ApplicationController < ActionController::Base
   around_action :switch_locale
+  before_action :load_group_info
+
+  private
 
   def switch_locale(&action)
     cookies[:locale] = params[:locale] if params[:locale]
 
     locale = cookies[:locale] || 'pt-BR'
     I18n.with_locale(locale, &action)
+  end
+
+  def load_group_info
+    @group = {
+      name: 'GEPTA'
+    }
   end
 end
