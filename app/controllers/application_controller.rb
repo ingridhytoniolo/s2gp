fe45bc588ec4af44f54dd041a9f532f5b96c2758
Renan Nelsen <renan@nelsen.com.br>
2021-10-05 22:23:15 -0300

    Add group info

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -13,7 +13,13 @@ class ApplicationController < ActionController::Base
 
   def load_group_info
     @group = {
-      name: 'GEPTA'
+      name: 'GEPTA',
+      long_name: 'Grupo de Estudos e Pesquisas em Tecnologia Aplicada',
+      logo_url: 'gepta-logo.jpg',
+      about: '
+        <p>O GEPTA é um grupo de pesquisa, liderado pelos Professores doutores Luiz Antônio Pereira Neves e José Elmar Feger, situado no Setor de Educação Profissional e Tecnológica, da UFPR, no campus Politécnico de Curitiba.</p>
+        <p>Este grupo desenvolve pesquisa acadêmica e aplicada na grande área Interdisciplinar na Universidade Federal do Paraná, tendo como foco principal promover pesquisas aplicadas nas linhas de processamento digital de imagens, engenharia biomédica e de reabilitação, biometria, sistemas embarcados, robótica, desenvolvimento de aplicações na WEB, tecnologias com dispositivos móveis e tecnologias para gestão da informação, aplicados na educação, na saúde e no turismo.<p>
+      '
     }
   end
 end
