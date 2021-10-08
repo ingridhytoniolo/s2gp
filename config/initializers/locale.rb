03cd86b44f6a2fe567a79a2e308645f925e85922
Renan Nelsen <renan@nelsen.com.br>
2021-10-08 12:15:11 -0300

    Change locale info

--- config/initializers/locale.rb
+++
@@ -1,5 +0,0 @@
-# Where the I18n library should search for translation files
-I18n.load_path += Dir[Rails.root.join('config', 'locales', '*.{rb,yml}')]
-
-# Permitted locales available for the application
-I18n.available_locales = [:en, :'pt-BR']
